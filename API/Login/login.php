4f8f7ea7fc6f746099384b548a1cd09de180aef1
JacopoSciampi <sciampi.jacopo@gmail.com>
2019-03-30 13:03:06 +0100

    Addind login first logic

---
+++ API/Login/login.php
@@ -0,0 +1,67 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    $json = file_get_contents('php://input');
+    $array = json_decode($json, true);
+
+    $username = $array['username'];
+
+    if(isset($username) && $array['password']){
+        require('../../function/const.php');
+        session_start();
+        $res = [];
+
+        $con = mysqli_connect($GLOBALS['DATABASE_HOST'], $GLOBALS['DATABASE_USER'], $GLOBALS['DATABASE_PASS'], $GLOBALS['DATABASE_NAME']);
+        if ( mysqli_connect_errno() ) {
+            $res = [
+                'status' =>  'KO',
+                'message' => 'System internal error'
+            ];
+
+            echo json_encode($res);
+            return;
+        }
+
+        if ($stmt = $con->prepare("SELECT username, password FROM users WHERE username = ?")) {
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if ($stmt->num_rows > 0) {
+                $stmt->bind_result($id, $password);
+                $stmt->fetch();
+                if (password_verify($array['password'], $password)) {
+                    $res = [
+                        'status' =>  'ok',
+                        'message' => 'Welcome back.'
+                    ];
+                
+                    echo json_encode($res);
+                } else {
+                    $res = [
+                        'status' =>  'error',
+                        'message' => 'Invalid login data.'
+                    ];
+                
+                    echo json_encode($res);
+                }
+            } else {
+                $res = [
+                    'status' =>  'error',
+                    'message' => 'Invalid login data.'
+                ];
+    
+                echo json_encode($res);
+            }
+            $stmt->close();
+        }
+
+    } else {
+        $res = [
+            'status' =>  'No data.',
+            'message' => 'No data found in the request.'
+        ];
+
+        echo json_encode($res);
+        //No data on the request
+    }
+?>
---
+++ function/const.php
@@ -0,0 +1,6 @@
+<?php
+    $GLOBALS['DATABASE_HOST'] = 'localhost';
+    $GLOBALS['DATABASE_USER'] = 'root';
+    $GLOBALS['DATABASE_PASS'] = '';
+    $GLOBALS['DATABASE_NAME'] = 'MedusaDB';
+?>
